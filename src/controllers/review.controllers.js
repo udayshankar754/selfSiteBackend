02bb5ce2f64d91f9d4345221e42050c48468bb91
UdayShankar754 <uday.naxtre@gmail.com>
2024-09-24 15:12:07 +0530

    added Review Routes

--- src/controllers/review.controllers.js
+++ src/controllers/review.controllers.js
@@ -1,5 +1,6 @@
 import { Project } from "../models/project.models.js";
 import { Review } from "../models/review.models.js";
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js"
 
@@ -8,7 +9,7 @@ const addReview = asyncHandler(async (req, res) => {
     const { name , designation , rating , review , email , mobileNumber , projectId } = req.body;
 
     if(
-        [ name , rating , review ,projectId].some((fields) => fields?.trim() === "")
+        [ name , review ,projectId].some((fields) => fields?.trim() === "")
     ) {
         throw new ApiError(400, "Please fill all the fields")
     }
@@ -17,13 +18,21 @@ const addReview = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Please provide an email or mobile number")
     }
 
-    const isValidProject = Project.findById(projectId);
+    if(parseFloat(rating)) {
+        if(rating < 1 || rating > 5) {
+            throw new ApiError(400, "Rating should be between 1 and 5")
+        } 
+    } else {
+        throw new ApiError(400, "Rating should be a number or a floating point number")
+    }
+
+    const isValidProject = await Project.findById(projectId);
 
     if(!isValidProject) {
         throw new ApiError(404, "Project not found")
     }
 
-    const newReview = Review.create({
+    const newReview = await Review.create({
         name,
         designation,
         rating,
@@ -34,7 +43,7 @@ const addReview = asyncHandler(async (req, res) => {
     })
 
     return res.status(201).json(
-        new ApiResponse(200 , "Review Created Successfully" , newReview)
+        new ApiResponse(200 , newReview ,"Review Created Successfully")
     ) 
     
 })
@@ -42,8 +51,70 @@ const addReview = asyncHandler(async (req, res) => {
 const editReview = asyncHandler(async (req ,res) => {
     const { id } = req.params;
     const { name, designation, rating, review, email, mobileNumber } = req.body;
+
+    if(!(name || mobileNumber || email || review || designation || rating)) {
+        throw new ApiError(400, "Please fill The field You want to edit")
+    }
+
+    if( email || mobileNumber) {
+        if(email.trim() == '' || mobileNumber.trim() == '') {
+            throw new ApiError(400, "Please provide an email or mobile number")
+        }
+    }
+
+    if(parseFloat(rating)) {
+        if(rating < 1 || rating > 5) {
+            throw new ApiError(400, "Rating should be between 1 and 5")
+        } 
+    } else {
+        throw new ApiError(400, "Rating should be a number or a floating point number")
+    }
+
+    const isValidReview = await Review.findById(id);
+    
+    if(!isValidReview) {
+        throw new ApiError(404, "Review not found")
+    }
+
+    const UpdateReview = await Review.findByIdAndUpdate(id , {
+        $set : {
+            name,
+            designation,
+            rating,
+            review,
+            email,
+            mobileNumber,
+        }
+    },
+    {
+        new : true,
+    }
+    )
+
+    return res.status(200).json(
+        new ApiResponse(200, UpdateReview, "Review updated successfully")
+    )
+ 
+})
+
+const deleteReview = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const isValidId = Review.findById(id);
+
+    if(!isValidId) {
+        throw new ApiError(404, "Review not found")
+    }
+
+    await Review.findByIdAndDelete(id);
+
+    return res.status(200).json(
+        new ApiResponse(200, null, "Review deleted successfully")
+    )
 })
 
 export {
     addReview, 
+    editReview,
+    deleteReview,  
 }
--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 
 import {verifyJWT} from "../middlewares/auth.middlewares.js"
-import { addReview } from '../controllers/review.controllers.js';
+import { addReview, deleteReview, editReview } from '../controllers/review.controllers.js';
 
 const router = Router();
 
 
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 router.route("/add-review").post(addReview)
-
-
+router.route("/update-review/:id").patch(editReview)
+router.route("/delete-review/:id").delete(deleteReview)
 
 export default router
